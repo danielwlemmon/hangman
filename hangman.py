d11a8790560621bd9b098a45174350ccfee14bce
Daniel Lemmon <95183188+danielwlemmon@users.noreply.github.com>
2022-10-05 17:10:20 -0600

    Initial commit

---
+++ hangman.py
@@ -0,0 +1,51 @@
+import random
+from site import USER_BASE
+from words import words
+import string
+
+
+def get_valid_word(words):
+    word = random.choice(words)
+    while '-' in word or ' ' in word:
+        word = random.choice(words)
+
+    return word
+
+
+def hangman():
+    word = get_valid_word(words)
+    word_letters = set(word)
+    alphabet = set(string.ascii_lowercase)
+    used_letters = set()
+
+    lives = 6
+
+    while len(word_letters) > 0 and lives > 0:
+        print('You have', lives, 'lives left and you have used these letter: ',
+              ' '.join(used_letters))
+
+        word_list = [
+            letter if letter in used_letters else '-' for letter in word]
+        print('Current word: ', ' '.join(word_list))
+
+        user_letter = input('Guess a letter: ').lower()
+
+        if user_letter in alphabet - used_letters:
+            used_letters.add(user_letter)
+            if user_letter in word_letters:
+                word_letters.remove(user_letter)
+            else:
+                lives = lives - 1
+
+        elif user_letter in used_letters:
+            print('You have already used that character. Please try again')
+        else:
+            print('Invalid character. Please try again.')
+
+    if (lives == 0):
+        print('You dies, sorry. The word was', word)
+    else:
+        print('You guessed the word', word, '!!')
+
+
+hangman()
